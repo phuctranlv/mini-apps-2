43a0c5147ead63ab935567c4755ac71eb8168b9c
Phuc Tran <phuctranlv@gmail.com>
2020-04-25 00:54:42 -0700

    Add indexedDB feature to client

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(port, () => { console.log(`listening on port ${port}...`); });
